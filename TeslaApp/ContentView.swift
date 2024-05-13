499b4f9c9d1042ce21f222d647c0344109265cc4
SonataGirl <Ino-sonata1988>
2024-05-13 22:52:21 +0300

    Initial Commit

---
+++ TeslaApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TeslaApp
+//
+//  Created by Sonata Girl on 13.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TeslaApp/TeslaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaAppApp.swift
+//  TeslaApp
+//
+//  Created by Sonata Girl on 13.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TeslaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
